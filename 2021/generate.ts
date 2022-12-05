1e5b36c3af21b685ecd1bc8982e93c460c15acf4
Johann Lochbaum <johann@loch-im-baum.de>
2022-12-05 16:59:36 +0100

    feat(2021): prepare plattform

---
+++ 2021/generate.ts
@@ -0,0 +1,89 @@
+import { mkdir, readdir, writeFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { toWords } from 'number-to-words';
+import { StringUtil } from '@shared/utils/string';
+
+async function getInputForDay(day: number): Promise<string> {
+  const sessionToken = process.env.SESSION_TOKEN;
+  if (!sessionToken) throw new Error('Env variable SESSION_TOKEN missing!');
+
+  const result = await fetch(`https://adventofcode.com/2021/day/${day}/input`, {
+    headers: {
+      cookie: `session=${sessionToken}`,
+    },
+  });
+
+  if (!result.ok) {
+    switch (result.status) {
+      case 400:
+        throw new Error(
+          'Authentication failed! Did you set the SESSION_TOKEN env properly?'
+        );
+
+      case 404:
+        throw new Error('Input not available yet :(');
+
+      default:
+        console.log(await result.text());
+        throw new Error('Unkown error :o');
+    }
+  }
+
+  return await result.text();
+}
+
+async function run() {
+  const doorsDirectory = resolve('./src/doors');
+
+  const existingDoors = (await readdir(doorsDirectory, { withFileTypes: true }))
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => parseInt(dirent.name));
+
+  const lastDoor = Math.max(...existingDoors) || 0;
+  const nextDoor = lastDoor + 1;
+  const nextDoorAsWord = toWords(nextDoor);
+
+  console.log('>> Generating next door #' + nextDoor);
+
+  console.log('Getting challenge input...');
+  const doorInput = await getInputForDay(nextDoor);
+
+  console.log('Generating files...');
+  const nextDoorDirectory = doorsDirectory + '/' + nextDoor;
+  await mkdir(nextDoorDirectory);
+  await writeFile(nextDoorDirectory + '/input.txt', doorInput);
+
+  const templateCode = `import { join } from 'node:path';
+import { AbstractDoor } from '../../lib/AbstractDoor';
+import { Logger } from '../../lib/Logger';
+
+export default class Door${StringUtil.toTitleCase(
+    nextDoorAsWord
+  )} extends AbstractDoor {
+    public async run() {
+      // ##### PREPARE
+      for await (const line of this.readFileByLineInterator(
+        join(__dirname, './input.txt')
+      )) {
+      }
+
+      // ##### PART 1
+      Logger.partHeader(1);
+      console.log('TODO');
+
+      // ##### PART 2
+      Logger.partHeader(2);
+      console.log('TODO');
+    }
+}
+  `;
+
+  await writeFile(nextDoorDirectory + '/index.ts', templateCode);
+}
+
+run().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
+
+export {};
---
+++ 2021/index.ts
@@ -0,0 +1,63 @@
+import { bold, italic, red, white } from 'colorette';
+import { Command } from 'commander';
+import { AbstractDoor } from '@shared/lib/AbstractDoor';
+import { Logger } from '@shared/lib/Logger';
+
+const printWidth = 64;
+
+function printTitle() {
+  const headlineModifier = (v: string) => bold(red(v));
+
+  console.clear();
+  console.log(headlineModifier('#'.repeat(printWidth)));
+  console.log(headlineModifier(' '.repeat(printWidth)));
+  Logger.centeredText(printWidth, 'Advent of Code 2021', headlineModifier);
+  Logger.centeredText(printWidth, 'by Johann Lochbaum', (v) =>
+    italic(headlineModifier(v))
+  );
+  console.log(headlineModifier(' '.repeat(printWidth)));
+  console.log(headlineModifier('#'.repeat(printWidth)));
+  console.log();
+}
+
+const program = new Command();
+
+program
+  .name('advent-of-code-2021')
+  .option('--door <number>', 'Door to run', '1');
+
+program.parse();
+
+async function run() {
+  printTitle();
+
+  const doorNumber = parseInt(program.opts().door);
+  if (Number.isNaN(doorNumber) || doorNumber < 1 || doorNumber > 24)
+    throw new Error('Not a valid door number!');
+
+  console.log(white(`Running door #${doorNumber}`));
+  console.log('-'.repeat(printWidth));
+  console.log();
+
+  const doorImport = await import('./doors/' + program.opts().door).catch(
+    () => undefined
+  );
+  if (!doorImport)
+    throw new Error(`Door ${doorNumber} is not implemented yet!`);
+  if (!doorImport.default)
+    throw new Error(`Door ${doorNumber} is missing a default export!`);
+
+  const door: AbstractDoor = new doorImport.default();
+
+  const startMs = Date.now();
+  await door.run();
+  const endMs = Date.now();
+  console.log(`It took a total of ${endMs - startMs}ms to run this door!`);
+}
+
+run().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
+
+export {};
