36f5554a65b2cc2daef97d8a0fed45d61704d461
Johann Lochbaum <johann@loch-im-baum.de>
2022-12-06 20:49:52 +0100

    day 6 - part 2

--- 2022/doors/6/index.ts
+++ 2022/doors/6/index.ts
@@ -3,13 +3,13 @@ import { AbstractDoor } from '@shared/lib/AbstractDoor';
 import { Logger } from '@shared/lib/Logger';
 import { StringUtil } from '@shared/utils/string';
 
-class Part1Analyzer {
+class Analyzer {
   constructor(private dataStream: string) {}
 
-  getIndexAfterMarker() {
-    // Because we require the packet to be 4 chars, we can start at the fourth one
-    for (let i = 3; i < this.dataStream.length; ++i) {
-      const lastFourChars = this.dataStream.slice(i - 3, i + 1);
+  getIndexAfterPacketWithXUniqueChars(x: number) {
+    // Skip positions that will result in undersize packages
+    for (let i = x - 1; i < this.dataStream.length; ++i) {
+      const lastFourChars = this.dataStream.slice(i - x + 1, i + 1);
 
       if (!StringUtil.containsDuplicateChars(lastFourChars)) return i + 1;
     }
@@ -23,11 +23,12 @@ export default class DoorSix extends AbstractDoor {
 
     // ##### PART 1
     Logger.partHeader(1);
-    const analyzerPart1 = new Part1Analyzer(signatData);
-    console.log(analyzerPart1.getIndexAfterMarker());
+    const analyzer = new Analyzer(signatData);
+    console.log(analyzer.getIndexAfterPacketWithXUniqueChars(4));
 
     // ##### PART 2
     Logger.partHeader(2);
-    console.log('TODO');
+    // We can simply resure part 1's analyzer
+    console.log(analyzer.getIndexAfterPacketWithXUniqueChars(14));
   }
 }
